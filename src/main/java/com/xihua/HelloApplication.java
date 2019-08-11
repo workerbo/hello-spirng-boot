58f4f6438bc0e73871e2976323c3f28cf3047ede
何波 <bo.he03@hand-china.com>
2019-08-11 08:43:58 +0800

    diyic

---
+++ src/main/java/com/xihua/HelloApplication.java
@@ -0,0 +1,14 @@
+package com.xihua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * create by workerbo on 2019/7/521:11
+ */
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(com.xihua.HelloApplication.class, args);
+    }
+}
